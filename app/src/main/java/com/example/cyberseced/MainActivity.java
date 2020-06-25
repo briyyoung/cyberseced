84fcbf8a83e849dfb570f280d9581e1b7a13d363
briyyoung <brianyoung555@gmail.com>
2020-06-25 12:40:18 +1000

    INTIAL NEW COMMIT

--- app/src/main/java/com/example/cyberseced/MainActivity.java
+++ app/src/main/java/com/example/cyberseced/MainActivity.java
@@ -16,53 +16,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String MESSAGE = "This is from MainActivity";
-    private static final String TAG = "";
-    private RecyclerView mRecyclerView;
-    private RecyclerView.LayoutManager mLayoutManager;
-    PostDatabase pDb;
-    private Button btnPost;
-    private TextInputLayout inputPost;
-    private MainActivityAdapter mAdapter;
-
-    public static int postID = 0;
-
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = findViewById(R.id.mainActivityView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        btnPost = findViewById(R.id.btnPost);
-        inputPost = findViewById(R.id.post_input);
-
-        //create database
-        pDb = Room.databaseBuilder(getApplicationContext(), PostDatabase.class, "post-database").build();
-
-        mAdapter = new MainActivityAdapter(this, pDb);
-        mRecyclerView.setAdapter(mAdapter);
-
-        btnPost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new StoreUserPost().execute();
-//                pDb.getPostDao().insert(new PostEntity(postID, inputPost.getEditText().getText().toString()));
-                postID++;
-            }
-        });
     }
 
-    private class StoreUserPost extends AsyncTask<String, Void, Void > {
-        @Override
-        protected Void doInBackground(String... ids) {
-            String userPost = String.valueOf(inputPost.getEditText().getText());
-            pDb.getPostDao().insert(new PostEntity(Integer.parseInt(ids[0]), ids[1]));
-            return null;
-        }
-    }
 }
--- app/src/main/java/com/example/cyberseced/PostDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.cyberseced;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {PostEntity.class}, version = 1)
-public abstract class PostDatabase extends RoomDatabase {
-    public abstract PostDao getPostDao();
-}
